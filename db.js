a78ac12b19d9ecb342404bc3739dab05f0f81c55
Pratanu Ghorui <ghoruipratanu@gmail.com>
2025-10-21 20:57:23 +0530

    change db link.

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 //const dburl='mongodb://localhost:27017/conference_management_system';
 
-// const dburl='mongodb+srv://ghoruipratanu:zdk0HiTR6lf42ZmF@conferencemanagementsys.m2uokxu.mongodb.net/';
-const dburl='mongodb+srv://chaudhurin18:ilD6q0QyjX8NYTmy@cluster0.mouglj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dburl='mongodb+srv://ghoruipratanu:zdk0HiTR6lf42ZmF@conferencemanagementsys.m2uokxu.mongodb.net/';
+// const dburl='mongodb+srv://chaudhurin18:ilD6q0QyjX8NYTmy@cluster0.mouglj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const mongodbUrl=dburl;
 mongoose.connect(mongodbUrl);
 const db=mongoose.connection;
